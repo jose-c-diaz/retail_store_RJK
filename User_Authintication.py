5f6b435caff46082975098dffe167b060b653aa2
ktara005 <55545542+ktara005@users.noreply.github.com>
2019-10-21 19:06:32 -0400

    Add files via upload

--- User_Authintication.py
+++ User_Authintication.py
@@ -1,5 +1,8 @@
 import psycopg2
 import getpass
+import hashlib
+import binascii
+import os
 
 
 # This establishes the connection to PG
@@ -7,34 +10,44 @@ try:
     conn = psycopg2.connect(
         database="RetailStore",
         user="postgres",
-        password="45928",
+        password="1234",
         host="127.0.0.1",
         port="5432"
     )
 
-# This function is suppose to make the user authen table on the PG Database
-    def createUserTable():
-        cursor = conn.cursor()
-        cursor.execute(
-            """CREATE TABLE IF NOT EXISTS user_authentication
-                (
-                    user_id SERIAL PRIMARY KEY,
-                    user_name TEXT NOT NULL,
-                    pass_word TEXT NOT NULL,
-                    admin_role BOOLEAN NOT NULL
-                )
-            """
-        )
-        conn.commit()
-        cursor.close()
-    createUserTable()
+
+# This creates the hash for the password
+
+    def hash_password(passW):
+        """Hash a password for storing."""
+        salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
+        pwdhash = hashlib.pbkdf2_hmac('sha512', passW.encode('utf-8'),
+                                      salt, 100000)
+        pwdhash = binascii.hexlify(pwdhash)
+        return (salt + pwdhash).decode('ascii')
+
+
+# This verifies the input password and hashed password match
+
+    def verify_password(stored_password, provided_password):
+        """Verify a stored password against one provided by user"""
+        salt = stored_password[:64]
+        stored_password = stored_password[64:]
+        pwdhash = hashlib.pbkdf2_hmac('sha512',
+                                      provided_password.encode('utf-8'),
+                                      salt.encode('ascii'),
+                                      100000)
+        pwdhash = binascii.hexlify(pwdhash).decode('ascii')
+        return pwdhash == stored_password
+
 
 # This function  is suppose to insert the info into the user authen table
-# the crypt() is suppose to hash the password once its inserted into the table
+
     def insertUserInfo(user, passW):
+        passW = hash_password(passW)
         cursor = conn.cursor()
         cursor.execute(
-            f"INSERT INTO user_authentication (user_name, pass_word, admin_role) VALUES ('{user}', '{passW}', 'false')"
+            f"INSERT INTO user_authentication (user_Name, pass_word, admin_role) VALUES ('{user}', '{passW}', 'false')"
         )
         conn.commit()
         print("New User registered successfully!")
@@ -43,25 +56,32 @@ try:
 
 # This function checks whether the user is an admin or user, and if their account exists
 
-
     def retrieveAllUserInfo(user, passW, role):
-        cursor = conn.cursor()
-        cursor.execute(
-            f"SELECT * FROM user_authentication WHERE user_name='{user}' AND pass_word='{passW}' AND admin_role='{role}'")
-        # rows = cursor.fetchall()
-        row = cursor.fetchall()
+        while True:
+            userNameInput = input("\nEnter your username >> ")
+            passwordInput = getpass.getpass("\nEnter your password >> ")
+            cursor = conn.cursor()
+            cursor.execute(
+                f"SELECT * FROM user_authentication WHERE user_Name='{userNameInput}' AND admin_role='{role}'")
+            row = cursor.fetchall()
+            cursor.close()
+            passhash = verify_password(row[0][2], passwordInput)
 
-        if row:
-            if(role == 'true'):
-                print(f"{user}, you have been authenticated. You're an admnistrator")
-            elif(role == 'false'):
-                print(f"{user}, you have been authenticated. You're a user")
-
-        else:
-            print("User does not exist!")
-        # else:
-        #     print("User does not exist")
-        cursor.close()
+            if row and passhash:
+                if(role == 'true'):
+                    print("\n~~~~~~~~~~~Succesful log in!~~~~~~~~~~~")
+                    print(
+                        f"\n\nWelcome {userNameInput} administrator! What would you like to do? ")
+                    break
+                elif(role == 'false'):
+                    print("\n~~~~~~~~~~~Succesful log in!~~~~~~~~~~~")
+                    print(
+                        f"\n\nWelcome {userNameInput}, what would you like to do today? ")
+                    break
+            else:
+                print(
+                    "Incorrect username and/or password, please check your credentials!")
+            return userNameInput, passwordInput
 
 
 except (Exception, psycopg2.Error) as error:
--- main2.py
+++ main2.py
@@ -1,18 +1,31 @@
 import os
 import getpass
 import User_Authintication
-from Customer import Customer
+from Customers import Customer
+import re
+import psycopg2
+
+# this is my string I used to compare to my birthinput
+regex = '[0-9/]$'
+
+# This clears the screen
 
 
 def clear(): return os.system('cls')
 
+# This is the function for registering the inputs of the new user
+
+
 def input_valid_name(fieldName):
     while True:
-        fieldInput = input("Enter your " + fieldName + " >> ")
+        fieldInput = input("\nEnter your " + fieldName + " >> ")
         if not fieldInput:
             print("Invalid " + fieldName + ", try again")
         else:
-            return fieldInput 
+            return fieldInput
+
+# The main function of our file
+
 
 def main():
     clear()
@@ -20,46 +33,66 @@ def main():
     print("---------------------- WELCOME TO OUR RJK RETAIL STORE --------------------------")
     print("\n\n")
 
+    print("\n~~~~~~~~~Please select an option below to make your experience at our store much more pleasant!~~~~~~~\n")
     answer = input(
-        "1- Sign in"
-        "\n2- Sign up "
-        "\n>>")
+        "1 - Sign in"
+        "\n2 - Sign up "
+        # "\n3 - Continue as Guest..."
+        "\n>> ")
     if answer[:1] == '1':
         # User Sign in
-        userNameInput = input("Enter your username >> ")
-        passwordInput = getpass.getpass("Enter your password >> ")
+        userHolder = 0
+        passholder = 0
         adminAssign = 'false'
         User_Authintication.retrieveAllUserInfo(
-            userNameInput, passwordInput, adminAssign)
-
-        print("Succesful log in!")
+            userHolder, passholder, adminAssign)
         customerObj = Customer()
-        customerObj.retrieveCustomerInfo(userNameInput)
+        customerObj.retrieveCustomerInfo(userHolder)
 
     elif answer[:1] == '2':
-
-        newUserInput = input("Enter a unique username! >> ")
+        # User Sign up
+        customerObj = Customer()
+        newUserInput = 0
+        newUserInput = customerObj.checkUsername(newUserInput)
         newPassInput = getpass.getpass(
-            "Enter a password with a minimum length of 15 characters! >> ")
+            "\nEnter a password! >> ")
         User_Authintication.insertUserInfo(newUserInput, newPassInput)
 
-        firstNameInput = input_valid_name("first name") 
-        lastNameInput = input_valid_name("last name") 
-        phoneInput = input_valid_name("phone number")
-        emailInput = input_valid_name("email address")
-        addressInput = input_valid_name("home address")
-        birthInput = input_valid_name("birthday")
+        firstNameInput = input_valid_name('first name')
+        lastNameInput = input_valid_name('last name')
 
-        customerObj = Customer()
-        customerObj.insertCustomerInfo(newUserInput, firstNameInput, lastNameInput, phoneInput, emailInput, addressInput, birthInput)
+        while True:
+            try:
+                phoneInput = int(input_valid_name('phone number'))
+                if phoneInput == int(phoneInput):
+                    break
+            except (Exception, ValueError):
+                print("Please enter only numbers!")
 
-        userNameInput = input("Enter your username >> ")
-        passwordInput = getpass.getpass("Enter your password >> ")
+        emailInput = input_valid_name('email address')
+        addressInput = input_valid_name('home address')
+
+        while True:
+            try:
+                birthInput = (input_valid_name(
+                    'birthday [format:(mm/dd/yyyy)]'))
+                if re.search(regex, birthInput):
+                    break
+                else:
+                    print("Please enter only numbers and format as: mm/dd/yyyy !")
+            except (Exception, psycopg2.Error):
+                print("Please enter correct format!")
+
+        activeAssign = 0
+        customerObj.insertCustomerInfo(
+            newUserInput, firstNameInput, lastNameInput, phoneInput, emailInput, addressInput, activeAssign, birthInput)
+
+        userNameInput = 0
+        passwordInput = 0
         adminAssign = 'false'
         User_Authintication.retrieveAllUserInfo(
             userNameInput, passwordInput, adminAssign)
 
-        print("Succesful log in!")
     else:
         print("Invalid entry\n")
 
