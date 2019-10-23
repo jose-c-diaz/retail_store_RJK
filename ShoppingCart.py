00d782d7414e67252916bd80b521bc2638fc207a
Jose C Diaz <55547156+jose920@users.noreply.github.com>
2019-10-22 22:53:51 -0400

    i've added my function

--- ShoppingCart.py
+++ ShoppingCart.py
@@ -11,15 +11,27 @@ def addProductToShppingCart(row, amount):
     shoppingArray.insert(a+1, ordersArray)
 
 
+def printTopBox(width: int):
+    print(u'\u2554' + u'\u2550'* width + u'\u2557')
+
+def printBottomBox(width: int):
+    print(u'\u255a' + u'\u2550'* width + u'\u255d')
+
+def printInBox(text: str, width: int):
+    spaceToFill = width - len(text)
+    print(u'\u2551' + text + " "*spaceToFill + u'\u2551')
+
 def printShoppingCart():
-    print(
-        f"---------- {len(shoppingArray)} Item/s in Your Shopping Cart ----------")
+    boxWidth = 100
+    printTopBox(boxWidth)
+    printInBox(f"---------- {len(shoppingArray)} Item/s in Your Shopping Cart ----------", boxWidth)
     totalPrice = 0
     for i in range(len(shoppingArray)):
         row = shoppingArray[i]
-        print(f"\n{i+1} - {row[3]}")
-        print(f"    {row[4]}")
-        print(f"    Price={row[5]}      X             Amount={row[6]}")
+        printInBox(f"{i+1} - {row[3]}", boxWidth)
+        printInBox(f"    {row[4]}", boxWidth)
+        printInBox(f"    Price={row[5]}      X             Amount={row[6]}", boxWidth)
         totalPrice = totalPrice + (float(row[5]) * float(row[6]))
-    print("-----------------------------------------------------")
-    print(f"Your Total is: $ {totalPrice}")
+    printInBox("-----------------------------------------------------", boxWidth)
+    printInBox(f"Your Total is: $ {totalPrice}", boxWidth)
+    printBottomBox(boxWidth)
